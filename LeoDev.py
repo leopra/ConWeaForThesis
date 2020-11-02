bfce18701375a4200d0e7ef68ecaa843d06ede0c
Leopra <Leopica1>
2020-11-02 12:02:50 +0000

    preparing for conwea

--- LeoDev.py
+++ LeoDev.py
@@ -54,4 +54,5 @@ k = dataforconwea[dataforconwea['label'] == 'Telecommunications & ICT'].sample(6
 z = dataforconwea[dataforconwea['label'] != 'Telecommunications & ICT']
 
 out = pd.concat([k,z])
-out.to_pickle('./data/eutopiavert/df.pkl', protocol=3)
+out = out.sample(frac=1).reset_index().drop('index', axis=1)
+out.to_pickle('./data/eutopiavert/df.pkl')
