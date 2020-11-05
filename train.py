d618f70352d2b43cecc24d3a7cff86792c92e7cf
Leopra <Leopica1>
2020-11-05 17:12:22 +0000

    debugging

--- train.py
+++ train.py
@@ -218,8 +218,14 @@ def main(dataset_path, print_flag=True):
         pred_labels = get_from_one_hot(pred, index_to_label)
 
         prednp = np.array(pred)
+        print(prednp)
+        print(type(prednp))
+        print(type(prednp[0]))
         print(prednp.shape)
         y_true_allnp = np.array(y_true_all)
+        print(y_true_allnp)
+        print(type(y_true_allnp))
+        print(type(y_true_allnp[0]))
         print(y_true_all.shape)
         from sklearn.metrics import confusion_matrix
         for i,l in enumerate(labels):
